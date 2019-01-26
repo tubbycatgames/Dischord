a8722c8236ba7c376a8a8a0d0a2f96564276785f
Phil Simmons <phil@tubbycatgames.com>
2019-01-26 12:46:28 -0500

    Add basic darkness scaler

---
+++ Assets/Scripts/DarknessScaler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarknessScaler : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject home;
+    public float idealDistance = 5;
+
+    private Light lt;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentDistance = Vector3.Distance(
+            home.transform.position,
+            player.transform.position
+        );
+
+        lt.intensity = Mathf.Min(
+            idealDistance/currentDistance, 
+            1
+        );
+    }
+}
