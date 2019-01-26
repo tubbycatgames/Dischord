564a5751a0b6f7491f9576f6f88dca7d1bed6dc0
Phil Simmons <phil@tubbycatgames.com>
2019-01-26 00:31:24 -0500

    Added Basic Movement Controller
    Added Script to handle movement by the user

---
+++ Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public float speed = 0.0f;
+    public float rotateSpeed = 0.0f;
+
+    private float yaw = 0.0f;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var moveDirection = new Vector3(
+            Input.GetAxis("Horizontal"),
+            0.0f,
+            Input.GetAxis("Vertical")
+        );
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection = moveDirection * speed;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        yaw += rotateSpeed * Input.GetAxis("Mouse X");
+        transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
+    }
+}
