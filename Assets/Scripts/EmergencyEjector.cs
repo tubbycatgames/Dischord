6b4bf0908db2f3908c9984a73159ecf8fb81d2f7
Phil Simmons <phil@tubbycatgames.com>
2019-01-27 09:49:48 -0500

    Added Reset to EmergencyEjector

--- Assets/Scripts/EmergencyEjector.cs
+++ Assets/Scripts/EmergencyEjector.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 class EmergencyEjector : MonoBehaviour
 {
+    private int rCount;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rCount++;
+            if (rCount == 3)
+            {
+                SceneManager.LoadScene("GameScene");
+            }
         }
     }
 }
