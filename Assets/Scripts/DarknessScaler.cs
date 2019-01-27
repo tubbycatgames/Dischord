bbf981c6ec72c3f1884e446edfee501260f1c984
Phil Simmons <phil@tubbycatgames.com>
2019-01-27 10:50:19 -0500

    Added in game fog and darkness scaling

--- Assets/Scripts/DarknessScaler.cs
+++ Assets/Scripts/DarknessScaler.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class DarknessScaler : MonoBehaviour
 {
+    public float MinSourceLight = 0f;
+    public bool ShowFog = true;
+
     private Light lt;
 
     void Start()
     {
         lt = GetComponent<Light>();
+        RenderSettings.fog = ShowFog;
     }
 
     void Update()
     {
-        lt.intensity = 1 - DissonanceManager.Instance.Score;
+        lt.intensity = Mathf.Lerp(
+            lt.intensity, 
+            1 - DissonanceManager.Instance.Score,
+            Time.deltaTime
+        );
     }
 }
