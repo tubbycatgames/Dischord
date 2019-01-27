026dc9429eea326c793b1acd301a7c9808571c5b
Phil Simmons <phil@tubbycatgames.com>
2019-01-27 08:43:37 -0500

    Tweaked Player to Hover over land

--- Assets/Scripts/CharacterMover.cs
+++ Assets/Scripts/CharacterMover.cs
@@ -6,6 +6,7 @@ public class CharacterMover : MonoBehaviour
 {
     public float speed = 0.0f;
     public float rotateSpeed = 0.0f;
+    public float hoverHeight = 0.0f;
 
     private float yaw = 0.0f;
 
@@ -26,6 +27,17 @@ public class CharacterMover : MonoBehaviour
         moveDirection = transform.TransformDirection(moveDirection);
         moveDirection = moveDirection * speed;
         controller.Move(moveDirection * Time.deltaTime);
+
+        var hit = new RaycastHit();
+        var terrain = Physics.Raycast(transform.position, -Vector3.up, out hit);
+        var position = transform.position;
+
+        var heightAdjustment = new Vector3(
+            position.x,
+            Mathf.Lerp(position.y, hit.point.y + hoverHeight, Time.deltaTime),
+            position.z
+        );
+        transform.position = heightAdjustment;
 
         yaw += rotateSpeed * Input.GetAxis("Mouse X");
         transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
