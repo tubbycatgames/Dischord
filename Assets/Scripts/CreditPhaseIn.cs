24b11dd6f898e36782e4836151dd4df7a0b910b3
Phil Simmons <phil@tubbycatgames.com>
2019-01-30 20:56:26 -0500

    Added Credits Phase In

---
+++ Assets/Scripts/CreditPhaseIn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditPhaseIn : MonoBehaviour
+{
+
+    public float FadeDuration;
+    public CanvasGroup Panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Panel.alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Panel.alpha = Mathf.Lerp(
+            0f,
+            1f,
+            Time.time / FadeDuration);
+    }
+}
